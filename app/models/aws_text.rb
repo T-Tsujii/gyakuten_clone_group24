97cfec5d259d3e5dfb9a22d13da6c8b849c1d274
YuichiTanaka <smile.yuichi.1013@gmail.com>
2020-07-02 13:43:54 +0900

    9_AWSテキスト教材のrakeタスクを実装

--- app/models/aws_text.rb
+++ app/models/aws_text.rb
@@ -1,4 +1,21 @@
 class AwsText < ApplicationRecord
     validates :title, presence: true
     validates :content, presence: true
+
+    def self.import(path)
+        list = []
+        CSV.foreach(path, headers: true) do |row|
+            list << {
+                title: row["title"],
+                content: row["content"],
+            }
+        end
+        begin
+            puts "インポート開始"
+            AwsText.create!(list)
+            puts "インポートに成功しました"
+        rescue ActiveModel::UnknownAttributeError => invalid
+            puts "インポートに失敗しました:#{invalid}"
+        end
+    end
 end
--- lib/tasks/import_csv.rake
+++ lib/tasks/import_csv.rake
@@ -1,2 +1,8 @@
-# CSVファイルを扱うために必要
-require 'import'
+namespace :import_csv do
+    # rake import_csv:aws_texts コマンドで
+    # aws_text_dataファイルからデータベースにインポートできます
+    desc "AwsText CSVデータのインポート"
+    task aws_texts: :environment do
+        AwsText.import('db/csv_data/aws_text_data.csv')
+    end
+end
