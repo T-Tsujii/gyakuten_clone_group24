9422907104c62bec8f58ee6200d1c6cf7ea4e2d5
smileyuichi <58574285+smileyuichi@users.noreply.github.com>
2020-07-07 17:58:46 +0900

    21_質問集ページの実装 (#23)
    * 21_質問集ページの実装

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
   devise_for :users
   root 'movies#index'
   resources :aws_texts, only: [:index]
+  resources :questions, only: [:index]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
