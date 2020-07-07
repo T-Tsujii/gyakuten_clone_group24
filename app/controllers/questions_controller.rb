c2ad1408dfc8b90e402d95b92deda1b5243436c9
YuichiTanaka <smile.yuichi.1013@gmail.com>
2020-07-07 17:39:12 +0900

    21_質問集ページの実装

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,10 +1,7 @@
 class QuestionsController < ApplicationController
-    before_action :authenticate_user!
 
     def index
         @questions = Question.all
-        # time = @questions.created_at
-        # @created_time = time.strftime("%Y 年%m 月%d 日")
     end
 
 end
