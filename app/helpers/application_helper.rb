b3634d5fcfdcdc36654a2914d0e745305c13abae
Sukitai <kitaitakayoshi@gmail.com>
2020-07-08 10:00:59 +0900

    defined class judge unction using application_helper.rb

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+  def base_container
+    if controller.controller_name == "movies"
+      return "video-base-container"
+    else 
+      return "base-container"
+    end
+  end
+
 end
