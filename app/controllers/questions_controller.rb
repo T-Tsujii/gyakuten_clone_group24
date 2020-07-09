25eb9244dba64444ef3c514818fb7e1a39291088
Sukitai <kitaitakayoshi@gmail.com>
2020-07-09 23:58:49 +0900

    activated flash indication function

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -2,12 +2,12 @@ class QuestionsController < ApplicationController
 
     def index
       @questions = Question.all
-      @new_question = Question.new
+      @question = Question.new
     end
 
     def create
       Question.create!(question_params)
-      redirect_to questions_path
+      redirect_to questions_path, notice: "質問を受け付けました。"
     end
 
     private
