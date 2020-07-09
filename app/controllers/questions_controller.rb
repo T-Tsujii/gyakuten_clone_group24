fd781eae80ab3d09871276835fb3bd9d9f72394e
Sukitai <kitaitakayoshi@gmail.com>
2020-07-09 22:36:59 +0900

    added posting function into app/views/questions/index.html.erb

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,11 +1,18 @@
 class QuestionsController < ApplicationController
 
     def index
-        @questions = Question.all
+      @questions = Question.all
+      @new_question = Question.new
     end
 
-    def new
-        @question = Question.new
+    def create
+      Question.create!(question_params)
+      redirect_to questions_path
     end
 
+    private
+
+    def question_params
+      params.require(:question).permit(:title, :detail)
+    end
 end
