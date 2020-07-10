bab132f456607502900c427a3b73d1bec3876823
Sukitai <kitaitakayoshi@gmail.com>
2020-07-11 08:49:36 +0900

    resolved conflict on route.rb

--- config/routes.rb
+++ config/routes.rb
@@ -5,10 +5,6 @@ Rails.application.routes.draw do
   devise_for :users
   root 'movies#index'
   resources :aws_texts, only: [:index]
-<<<<<<< HEAD
-  resources :questions, only: [:index, :create]
-=======
-  resources :questions, only: [:index, :show]
->>>>>>> master
+  resources :questions, only: [:index, :show, :create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
