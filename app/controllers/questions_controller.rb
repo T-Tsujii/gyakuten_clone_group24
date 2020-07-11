aabe138a50d0f6003510340b3b321ca0763c5acd
Sukitai <kitaitakayoshi@gmail.com>
2020-07-11 12:40:33 +0900

    activated the flash indicating error

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,13 +1,22 @@
 class QuestionsController < ApplicationController
 
     def index
-      @questions = Question.all
-      @question = Question.new
+      if flash[:params].blank?
+        @questions = Question.all
+        @question = Question.new
+      else
+        @questions = Question.all
+        @question = Question.new(flash[:params])
+      end
     end
 
     def create
-      Question.create!(question_params)
-      redirect_to questions_path, notice: "質問を受け付けました。"
+      post = Question.new(question_params)
+      if post.save
+        redirect_to questions_path, notice: "質問を受け付けました。" and return
+      else
+        redirect_to questions_path, flash: {params: question_params, alert: "必須項目は全て入力してください。"}
+      end
     end
 
     def show
