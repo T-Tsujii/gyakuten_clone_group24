624ad72ca4c028b89dcceba720f856c992e08a2b
YuichiTanaka <smile.yuichi.1013@gmail.com>
2020-07-16 18:01:58 +0900

    25_fixed_バリデーションの削除

--- app/models/ruby_rails_text.rb
+++ app/models/ruby_rails_text.rb
@@ -1,6 +1,5 @@
 class RubyRailsText < ApplicationRecord
     validates :genre, presence: true
-    validates :image, presence: true
     validates :title, presence: true
     validates :content, presence: true
 end
