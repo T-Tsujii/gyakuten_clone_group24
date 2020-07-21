9579cb34c396679c0a42f19d77b81ee5b78eea45
Sukitai <kitaitakayoshi@gmail.com>
2020-07-21 23:11:38 +0900

    created texts_controller and texts_index_view

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,6 @@ Rails.application.routes.draw do
   resources :questions, only: [:index, :show, :create] do
     resources :solutions, only: [:index, :create]
   end
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  resources :texts, only: %i[index]
 end
