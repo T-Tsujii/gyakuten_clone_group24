de91ce6dc4b1d15d0fc6cec829ff302e9b1119aa
Sukitai <kitaitakayoshi@gmail.com>
2020-07-27 12:54:02 +0900

    modified filtering.js to conform to ES5 and jQuery variable declaration

--- app/javascript/packs/filtering.js
+++ app/javascript/packs/filtering.js
@@ -1,14 +1,14 @@
-$(function(){
+$(document).on('turbolinks:load', function() {
   $("#search_box").keyup(function(){
-    var myInput = $(this).val().toLowerCase();
-    if(!myInput) {
-      $("#top-wrapper div.lesson-card").show();
+    var $myInput = $(this).val().toLowerCase();
+    var $lessonCard = $("#top-wrapper div.lesson-card");
+    if(!$myInput) {
+      $lessonCard.show();
     } else {
-      $("#top-wrapper div.lesson-card").hide();
-      var elems = $("#top-wrapper div.lesson-card");
+      $lessonCard.hide();
 
-      elems.each(function() {
-        if ($(this).find(".card-title").text().toLowerCase().indexOf(myInput) > -1 ) {
+      $lessonCard.each(function() {
+        if ($(this).find(".card-title").text().toLowerCase().indexOf($myInput) > -1 ) {
           $(this).show();
         }
       });
