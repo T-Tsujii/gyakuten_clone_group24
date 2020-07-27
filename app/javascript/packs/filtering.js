0370ff330a57df8030cfe3be4da7dada934cf78e
Sukitai <kitaitakayoshi@gmail.com>
2020-07-27 22:39:49 +0900

    modified javascript codes to conform to ES6 except for jQuery codes

--- app/javascript/packs/filtering.js
+++ app/javascript/packs/filtering.js
@@ -1,17 +1,20 @@
 $(document).on('turbolinks:load', function() {
-  $("#search_box").keyup(function(){
-    var $myInput = $(this).val().toLowerCase();
-    var $lessonCard = $("#top-wrapper div.lesson-card");
+  $("#search_box").keyup(function() {
+    
+    const $myInput = $(this).val().toLowerCase();
+    const $lessonCard = $("#top-wrapper div.lesson-card");
+
     if(!$myInput) {
       $lessonCard.show();
     } else {
       $lessonCard.hide();
-
-      $lessonCard.each(function() {
-        if ($(this).find(".card-title").text().toLowerCase().indexOf($myInput) > -1 ) {
-          $(this).show();
+      $lessonCard.each((i, element) => {
+        if ($(element).find(".card-title").text().toLowerCase().includes($myInput) ) {
+          $(element).show();
         }
       });
     }
   });
 });
+
+
