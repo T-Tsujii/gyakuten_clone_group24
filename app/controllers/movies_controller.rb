04880ecb83fca6efeb74794ed73baa990de361d5
YuichiTanaka <smile.yuichi.1013@gmail.com>
2020-07-30 20:39:11 +0900

    movies_controllerのindexアクションの修正

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,11 +1,8 @@
 class MoviesController < ApplicationController
 
     def index
-      if params[:category] == "Money"
-        movies = Movie.where(category: "Money").order(id: "ASC")
-        @movies = movies.page(params[:page]).per(12)
-      elsif params[:category] == "Writing"
-        movies = Movie.where(category: "Writing").order(id: "ASC")
+      if params[:category].present?
+        movies = Movie.where(category: params[:category]).order(id: "ASC")
         @movies = movies.page(params[:page]).per(12)
       else
         movies = Movie.where(category: nil).order(id: "ASC")
