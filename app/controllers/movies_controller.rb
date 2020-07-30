2754bd206d2eee9ac8ac140be6abc533b790ec07
YuichiTanaka <smile.yuichi.1013@gmail.com>
2020-07-30 18:11:02 +0900

    ライティング講座のページを実装しました

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -4,6 +4,9 @@ class MoviesController < ApplicationController
       if params[:category] == "Money"
         movies = Movie.where(category: "Money").order(id: "ASC")
         @movies = movies.page(params[:page]).per(12)
+      elsif params[:category] == "Writing"
+        movies = Movie.where(category: "Writing").order(id: "ASC")
+        @movies = movies.page(params[:page]).per(12)
       else
         movies = Movie.where(category: nil).order(id: "ASC")
         @movies = movies.page(params[:page]).per(12)
