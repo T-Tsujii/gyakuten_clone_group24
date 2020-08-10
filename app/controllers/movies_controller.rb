23713228eb538e1d328780564fc9d2c16faa3ce8
Sukitai <kitaitakayoshi@gmail.com>
2020-08-10 17:42:34 +0900

    redefined appearance on movies/index.html.erb

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -5,8 +5,8 @@ class MoviesController < ApplicationController
         movies = Movie.where(category: params[:category]).order(id: "ASC")
         @movies = movies.page(params[:page]).per(12)
       else
-        movies = Movie.where(category: nil).order(id: "ASC")
-        @movies = movies.page(params[:page]).per(12)
+        @movies_all = Movie.where(category: "Basic").order(id: "ASC")
+        @movies = @movies_all.page(params[:page]).per(12)
       end
     end
 
