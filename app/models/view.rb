b9a7d179de432fb2d90e56e7c30119d602e71eeb
YuichiTanaka <smile.yuichi.1013@gmail.com>
2020-08-13 08:59:33 +0900

    バリデーションの修正

--- app/models/view.rb
+++ app/models/view.rb
@@ -1,5 +1,5 @@
 class View < ApplicationRecord
-  validates :user_id, presence: true, uniqueness: true
+  validates :user_id, presence: true, uniqueness: {scope: :question_id}
   validates :question_id, presence: true
 
   belongs_to :user
